67811cec06be28d0dc02be8f129a65b6d61ba22f
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-04-21 23:43:34 -0500

    feat(pages/book/index.js): Primera query a prisma
    Se configura el componente para realizar la query de los libros
    Se renderiza información.

--- pages/books/index.js
+++ pages/books/index.js
@@ -1,5 +1,54 @@
 import React from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+import Book from "../../components/commons/Book";
+
+// No se pueden usar string para las queries, se necesita graphql-tag
+const ALL_BOOKS_QUERY = gql`
+	query ALL_BOOKS_QUERY {
+		books {
+			title
+			createdAt
+			id
+		}
+	}
+`;
 
 export default function Books() {
-	return <div>Books!</div>;
+	return (
+		<div className={"container"}>
+			<Query query={ALL_BOOKS_QUERY}>
+				{({ loading, error, data }) => {
+					if (error) return <>{error.message}</>;
+					if (loading) {
+						return <>loading...</>;
+					} else {
+						{
+							return (
+								<div className={"row"}>
+									{data.books.map(
+										({ title, id, thumbnail }) => (
+											<div
+												className={
+													"col-12 col-sm-4 col-md-3 col-lg-2"
+												}
+												key={id}
+											>
+												<Book
+													title={title}
+													id={id}
+													thumbnail={thumbnail}
+												/>
+											</div>
+										)
+									)}
+								</div>
+							);
+						}
+					}
+				}}
+			</Query>
+		</div>
+	);
 }
