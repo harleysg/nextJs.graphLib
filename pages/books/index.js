c9cf3e54f2c0bd517d5adda9012b01a5dfb1405d
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-04-27 00:21:15 -0500

    refactor(pages/books/index.js): ajuste estructura
    Se agrega función para navegar, usa Router de nextjs
    Se mejora el loading

--- pages/books/index.js
+++ pages/books/index.js
@@ -1,46 +1,48 @@
 import React from "react";
 import { Query } from "react-apollo";
-import gql from "graphql-tag";
+import Router from "next/router";
 
 import Book from "../../components/commons/Book";
-
-// No se pueden usar string para las queries, se necesita graphql-tag
-const ALL_BOOKS_QUERY = gql`
-	query ALL_BOOKS_QUERY {
-		books {
-			title
-			createdAt
-			id
-		}
-	}
-`;
+import { ALL_BOOKS_QUERY } from "../../queries/books.queries.js";
 
 export default function Books() {
+	function navigateToDetailPage(e, id) {
+		e.preventDefault();
+		Router.push(`/books/detail?id=${id}`);
+	}
 	return (
 		<div className={"container"}>
 			<Query query={ALL_BOOKS_QUERY}>
 				{({ loading, error, data }) => {
 					if (error) return <>{error.message}</>;
 					if (loading) {
-						return <>loading...</>;
+						return (
+							<div className={"c-grid-book"}>
+								{"abcdefghij".split("").map((demo) => (
+									<Book key={demo} loading={true} />
+								))}
+							</div>
+						);
 					} else {
 						{
 							return (
-								<div className={"row"}>
+								<div className={"c-grid-book"}>
 									{data.books.map(
 										({ title, id, thumbnail }) => (
-											<div
-												className={
-													"col-12 col-sm-4 col-md-3 col-lg-2"
+											<a
+												onClick={(e) =>
+													navigateToDetailPage(e, id)
 												}
+												href={`#`}
 												key={id}
 											>
 												<Book
 													title={title}
 													id={id}
 													thumbnail={thumbnail}
+													loading={false}
 												/>
-											</div>
+											</a>
 										)
 									)}
 								</div>
