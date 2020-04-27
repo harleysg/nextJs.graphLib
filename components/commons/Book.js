281a41235ce1524cb11e024384ca25c9c18c5621
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-04-27 00:15:16 -0500

    refactor(Book.js): ajsute estructura componente Book

--- components/commons/Book.js
+++ components/commons/Book.js
@@ -1,25 +1,31 @@
 import React from "react";
 
-export default function Book({ id, title, thumbnail }) {
-	function navigateToDetail(e, id) {
-		e.preventDefault();
-		console.log(`navigateToDetail -> id`, id);
-	}
-	return (
-		<>
+const imgDefault =
+	"https://a5e8126a499f8a963166-f72e9078d72b8c998606fd6e0319b679.ssl.cf5.rackcdn.com/images/preloaded_product_box.gif";
+
+export default function Book({ id, title, thumbnail, image, loading }) {
+	if (loading) {
+		return (
+			<div className={"c-book is-loading"}>
+				<img
+					src={imgDefault}
+					alt={"lading"}
+					className={"img-fluid c-book__thumbnail--image"}
+				/>
+			</div>
+		);
+	} else {
+		return (
 			<div className={"c-book"}>
 				<div className={"c-book__thumbnail"}>
-					<a onClick={(e) => navigateToDetail(e, id)} href="#">
-						<img
-							src={thumbnail}
-							alt={title}
-							className={"img-fluid c-book__thumbnail--image"}
-						/>
-					</a>
+					<img
+						src={image || thumbnail || imgDefault}
+						alt={title}
+						className={"img-fluid c-book__thumbnail--image"}
+					/>
 				</div>
 				<h4 className={"c-book__title"}>{title}</h4>
-				{/* <DeleteBook id={book.id}>x</DeleteBook> */}
 			</div>
-		</>
-	);
+		);
+	}
 }
