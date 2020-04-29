3b0e5b3cb80b79b8e30f4e8e9e31ed17b5ec941b
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-04-28 20:40:55 -0500

    feat(_document.js): new custom document

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class appDocument extends Document {
+	static async getInitialProps(ctx) {
+		const initialProps = await Document.getInitialProps(ctx);
+		return { ...initialProps };
+	}
+	render() {
+		return (
+			<Html>
+				<Head></Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
